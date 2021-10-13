f6d157e7e632f028c109b3e36da26a3e68bc6e01
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-12 22:13:01 -0400

    footer component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Footer from "./src/components/Footer";
+import Navbar from "./src/components/Navbar";
+
 export default function Home() {
-	return <div>Hello</div>;
+	return (
+		<div>
+			<Navbar />
+			<h1>Hello</h1>
+			<Footer />
+		</div>
+	);
 }
---
+++ pages/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+	return <div>&#169; 2021 People Listing | All Rights Reserved</div>;
+};
+
+export default Footer;
