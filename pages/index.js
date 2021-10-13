73eeec8389d4f20e7756db2e7582ffeed788643d
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-12 22:49:26 -0400

    added link to homepage component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Footer from "./src/components/Footer";
 import Navbar from "./src/components/Navbar";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div>
 			<Navbar />
 			<h1>Hello</h1>
+			<Link href="/people-listing">
+				<a>See People Listing</a>
+			</Link>
 			<Footer />
 		</div>
 	);
