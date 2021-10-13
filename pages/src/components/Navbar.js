de3fe13ef0d4039ca0469b2b29ba3a8cc131d46b
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-12 22:05:48 -0400

    created a navbar component

---
+++ pages/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav>
+			<div>
+				<h1>People Listing</h1>
+			</div>
+			<a>Home</a>
+			<a>About</a>
+			<a>People Listing</a>
+		</nav>
+	);
+};
+
+export default Navbar;
