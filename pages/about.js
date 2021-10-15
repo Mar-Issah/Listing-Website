ebde57b77b16a2b717d247e6c6e7052dfbf8c451
Mar-Issah <masy370@gmail.com>
2021-10-14 21:35:59 -0400

    added people page

--- pages/about.js
+++ pages/about.js
@@ -4,11 +4,13 @@ const About = () => {
 	return (
 		<div>
 			<h1>About</h1>
+			<br />
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
 				veniam, quis nostrud exercitation ullamco
 			</p>
+			<br />
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
