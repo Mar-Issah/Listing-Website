191eba0a1758b8083c6298f32672bb1be2dc4f2d
Mar-Issah <masy370@gmail.com>
2021-10-15 19:29:34 -0400

    implemented the getStaticProps

--- pages/people.js
+++ pages/people.js
@@ -1,5 +1,14 @@
 import Head from "next/head";
-const People = () => {
+
+export const getStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+
+	return {
+		props: { people: data },
+	};
+};
+const People = ({ people }) => {
 	return (
 		<>
 			<Head>
@@ -7,7 +16,8 @@ const People = () => {
 				<meta name="keywords" content="people" />
 			</Head>
 			<div>
-				<h1>All People</h1>
+				<h1>All People </h1>
+				{people.map((person) => person)}
 			</div>
 		</>
 	);
