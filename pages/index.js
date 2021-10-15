e86f411e8b0846e5062dd980c6434ebc4f098b9a
Mar-Issah <masy370@gmail.com>
2021-10-14 21:23:36 -0400

    css styles module

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
-		<div>
+		<div className={styles.container}>
 			<h1>Hello</h1>
 			<Link href="/people-listing">
 				<a>See People Listing</a>
