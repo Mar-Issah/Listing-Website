9472d65989bbf15379b8231933a8896a8195c1ca
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-14 20:18:40 -0400

    creating a layout component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import Footer from "../src/components/Footer";
-import Navbar from "../src/components/Navbar";
 import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div>
-			<Navbar />
 			<h1>Hello</h1>
 			<Link href="/people-listing">
 				<a>See People Listing</a>
 			</Link>
-			<Footer />
 		</div>
 	);
 }
