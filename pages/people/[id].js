3c4028b6a740d6ae63b1b7dd8036c35ada780832
Mar-Issah <masy370@gmail.com>
2021-11-01 18:47:58 -0400

    map over and return paths and fallback

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -6,13 +6,16 @@ export const getStaticPaths = async () => {
 	// return {
 	// 	paths: [ {params: {id}},{},{},{}]
 	// }
-	// map data to an array of path objects with params (id)
+	// map data to an array of path objects with params (id).
 	const paths = data.map((people) => {
 		return {
 			params: { id: people.id.toString() },
 		};
 	});
 
+	// so return the paths fxn above which will be saved in paths. basically the id will now be stored in paths and used in jsx. when the user clicks on the each person it will look through the paths array and pick the id and return a page using the params we created ie [id]
+
+	//fallback: something on which one can fall back to fall back on something. for now lets use false which will call the 404 page when the page is not found
 	return {
 		paths,
 		fallback: false,
