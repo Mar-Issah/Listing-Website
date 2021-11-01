d63123f9cdc99754b6ea845af2b7ccb3601fa04a
Mar-Issah <masy370@gmail.com>
2021-11-01 18:31:47 -0400

    map over the getStaticPaths to return id

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -2,6 +2,10 @@ export const getStaticPaths = async () => {
 	const res = await fetch("https://jsonplaceholder.typicode.com/users");
 	const data = await res.json();
 
+	//we could write it this way but it is tedious to return each id for every pesrson so below is more concise as it map over the data and return the id
+	// return {
+	// 	paths: [ {params: {id}},{},{},{}]
+	// }
 	// map data to an array of path objects with params (id)
 	const paths = data.map((people) => {
 		return {
